2bc31ba5a9548129e300959817c8ab3580bb2cc0
kinng4 <33288313+kinng4@users.noreply.github.com>
2018-11-23 15:01:10 -0500

    added the vote report and download functionality

--- finalproject/votes.rb
+++ finalproject/votes.rb
@@ -1,5 +1,6 @@
 require 'dm-core'
 require 'dm-migrations'
+require 'csv'
 
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 
@@ -31,13 +32,17 @@ post '/vote' do
 
   vote = Voting.get(session[:id].username)
 
+  #Check if the current student has already voted
   if vote
     redirect to('/vote')
   else
+    # Check that all entries from the drop-down menus are different
     if params[:first_place] != params[:second_place] && params[:first_place] != params[:third_place] && params[:second_place] != params[:third_place]
+      #Create a vote for the logged-in user
       Voting.create(username: session[:id].username, first_place: params[:first_place],
                     second_place: params[:second_place], third_place: params[:third_place])
 
+      #Modify vote_bit to be one to indicate that the user has voted
       user = User.get(session[:id].username)
       user.vote_bit = 1
       user.save
@@ -48,6 +53,37 @@ post '/vote' do
       @sites_users = Site.all
       @sites_count = Site.count
       slim :vote_form
+    end
+  end
+end
+
+get '/report' do
+
+  if session[:id]
+    instructor = User.get(session[:id].username)
+    halt(401, 'Not Authorized') unless instructor.role == 'instructor'
+    @votes = Voting.all
+    @votes_count = Voting.count
+    slim :vote_report
+  else
+    halt(401, 'Not Authorized')
+  end
+end
+
+
+post '/report/download' do
+  #Get all records in the voting database
+  report = Voting.all
+
+  #Set the download file name and type
+  content_type 'application/csv'
+  attachment 'report.csv'
+
+  #Create a .csv string for download
+  CSV.generate do |csv|
+    csv << ["Who Voted", "First Place", "Second Place", "Third Place"]
+    report.each do |vote|
+      csv << [vote.username, vote.first_place, vote.second_place, vote.third_place]
     end
   end
 end
